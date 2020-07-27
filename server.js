d9033e865e1ec6749cd654bf24879e1ee1976d62
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-07-27 07:26:43 -0500

    ¿Cómo pueden venir las peticiones?

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const router = express.Router();
+
 const app = express();
 
-app.use("/", function (req, res) {
-  res.send("hola");
+app.use(router);
+
+router.get("/", function (req, res) {
+  res.send("Lista de mensajes");
 });
+
+router.post("/message", function (req, res) {
+  res.send("Mensaje añadido");
+});
+
+// app.use("/", function (req, res) {
+//   res.send("hola");
+// });
 
 app.listen(3000);
 console.log("La aplicación esta escuchando en http://localhost:3000");
