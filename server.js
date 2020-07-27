a6ed16bae38a977e08e6067395361d840acee22c
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-07-27 07:37:58 -0500

    Recibir información desde el cliente: Body y Query

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
 
 const app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
 router.get("/", function (req, res) {
   res.send("Lista de mensajes");
 });
 
-router.post("/message", function (req, res) {
-  res.send("Mensaje añadido");
-});
+router.delete("/message", function (req, res) {
+  console.log("req", req.query);
+  console.log("req", req.body);
 
-// app.use("/", function (req, res) {
-//   res.send("hola");
-// });
+  res.send("Mensaje " + req.body.text + " añadido");
+});
 
 app.listen(3000);
 console.log("La aplicación esta escuchando en http://localhost:3000");
