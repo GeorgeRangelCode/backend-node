1f96bbb785d8dcb7129be4e2543c891ab1afbb19
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-07-28 17:16:11 -0500

    MongoDB: Almacenar y leer datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,25 @@
-const list = [];
+const db = require("mongoose");
+const Model = require("./model");
+/**
+ * mongodb+srv://db_user_backend_node:db_user_backend_node@cluster0.ocf3j.mongodb.net/telegrom?retryWrites=true&w=majority
+ */
+db.Promise = global.Promise;
+db.connect(
+  "mongodb+srv://db_user_backend_node:db_user_backend_node@cluster0.ocf3j.mongodb.net/telegrom?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
+
+console.log("[db] Conectada con éxito");
 
 function addMessage(message) {
-  list.push(message);
+  // list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessages() {
-  return list;
+async function getMessages() {
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
