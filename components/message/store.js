0000967c3460928431468925bc37cc4a5c7bda03
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-07-29 16:42:32 -0500

    MongoDB: Consultar datos

--- components/message/store.js
+++ components/message/store.js
@@ -16,8 +16,13 @@ function addMessage(message) {
   myMessage.save();
 }
 
-async function getMessages() {
-  const messages = await Model.find();
+async function getMessages(filterUser) {
+  let filter = {};
+
+  if (filterUser !== null) {
+    filter = { user: filterUser };
+  }
+  const messages = await Model.find(filter);
   return messages;
 }
 
