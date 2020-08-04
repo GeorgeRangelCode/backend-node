5c51f5671e1f3778b961d86be6929c2193f08311
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-08-04 06:34:27 -0500

    Listado de usuarios

--- components/user/network.js
+++ components/user/network.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const response = require("../../network/response");
 const controller = require("./controller");
+const { route } = require("../message/network");
 const router = express.Router();
 
 router.post("/", function (req, res) {
@@ -8,6 +9,17 @@ router.post("/", function (req, res) {
     .addUser(req.body.name)
     .then((data) => {
       response.success(req, res, data, 201);
+    })
+    .catch((e) => {
+      response.error(req, res, "Internal Error", 500, e);
+    });
+});
+
+router.get("/", function (req, res) {
+  controller
+    .listUsers()
+    .then((users) => {
+      response.success(req, res, users, 200);
     })
     .catch((e) => {
       response.error(req, res, "Internal Error", 500, e);
